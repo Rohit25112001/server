09b0c13330c494c3080a2c4382cae3448cfdc667
Rohit <rohit.ku25112001@gmail.com>
2024-04-14 20:42:04 +0530

    new

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
   origin: '*',
   credentials: true,
-  methods:['GET','POST','PUT','DELETE']
+  methods:["GET","POST","PUT","DELETE"]
 }))
 
 // app.options('*', cors());
