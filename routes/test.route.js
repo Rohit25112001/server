33302440bbd917a10f794917265992471f43cb44
Rohit <rohit.ku25112001@gmail.com>
2024-04-15 01:06:12 +0530

    new

--- routes/test.route.js
+++ routes/test.route.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res)=>{
+    // const session = jwt.sign(payload(user),process.env.DATA_SECRET_KEY);
+    res.cookies('testing','hello',{
+        httpOnly:true,
+            maxAge:604800000,
+            secure:true
+    })
     res.status(200).json({message:'testing'})
 })
 
