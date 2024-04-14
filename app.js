c3f5ddc52585d26a7fb0beef548fa585c0700394
Rohit <rohit.ku25112001@gmail.com>
2024-04-14 21:33:58 +0530

    new

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const corsConfig = {
-  origin: '*',
+  origin: 'https://rentmojo-eight.vercel.app/',
   credentials: true,
   methods:["GET","POST","PUT","DELETE"]
 }
