1365b6cb9700aca3a79b9f6c609e6fa67b25ac69
Rohit <rohit.ku25112001@gmail.com>
2024-04-15 16:59:19 +0530

    add user

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -33,7 +33,7 @@ const signup = async (req, res) =>{
 
 const login = async (req, res) => {
     const user = await User.findOne({email:req.query.email},{createdAt:0,updatedAt:0,__v:0});
-    res.status(200).json({data:user})
+    res.status(200).json({data:'hello'})
     // try{
     //     const user = await User.findOne({email:req.query.email},{createdAt:0,updatedAt:0,__v:0});
 
