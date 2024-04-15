19245807ca405ba04a18c0365fc38a73cdc42624
Rohit <rohit.ku25112001@gmail.com>
2024-04-15 20:42:20 +0530

    add user

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -38,7 +38,6 @@ const login = async (req, res) => {
         res.cookie('testing', 'hello', {
             httpOnly: true,
             maxAge: 604800000, 
-            sameSite: 'none', 
             secure: true 
         });
         res.status(200).json({data:user})
