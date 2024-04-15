ab19b48bd2d6a4986059935cbbff57359327222e
Rohit <rohit.ku25112001@gmail.com>
2024-04-15 18:26:09 +0530

    add user

--- controllers/user/user.controller.js
+++ controllers/user/user.controller.js
@@ -32,60 +32,60 @@ const signup = async (req, res) =>{
 }
 
 const login = async (req, res) => {
-    const user = await User.findOne({email:req.query.email},{createdAt:0,updatedAt:0,__v:0});
-    // try{
-    //     const user = await User.findOne({email:req.query.email});
-    
-    //     res.status(200).json({data:user})
-    // }
-    // catch(err){
-    //     res.send(err)
-    // }
+    // const user = await User.findOne({email:req.query.email},{createdAt:0,updatedAt:0,__v:0});
     try{
-        const user = await User.findOne({email:req.query.email},{createdAt:0,updatedAt:0,__v:0});
-
-        if(!user) return res.status(404).json({success:false})
-
-        const checkPass = bcrypt.compare(req.query.password,user.password);
-
-        if(!checkPass) return res.status(401).json({success:false})
-
-        const payload = (user) => {
-            return {
-                id: user._id,
-                fullname: user.fullname,
-                email: user.email,
-                mobile: String(user.mobile).replace(/\d(?=1\d{2})/g, '*'),
-                createdAt: user.createdAt,
-            };
-        };
-        
-        const session = jwt.sign(payload(user),process.env.DATA_SECRET_KEY);
-        const {at , rt} = getCookies(undefined, 'all', process.env.DATA_SECRET_KEY ,user); // calling from cookie module
-
-        res.cookie('rt',rt,{
-            httpOnly:true,
-            maxAge:604800000,
-            sameSite:'none',
-            secure:true
-        })
+        const user = await User.findOne({email:req.query.email});
     
-        res.cookie('at',at,{
-            httpOnly:true,
-            maxAge:900000,
-            sameSite:'none',
-            secure:true
-        })
-        const currentTime = moment();
-        currentTime.add(12, 'minutes');
-        const current_time = btoa(currentTime);
-        console.log(current_time);
-        res.status(200).json({data:session,time:current_time})
+        res.status(200).json({data:user})
     }
     catch(err){
-        console.log(err);
-        res.status(500).json({success:false,message: 'Internal Server Error' })
+        res.send(err)
     }
+    // try{
+    //     const user = await User.findOne({email:req.query.email},{createdAt:0,updatedAt:0,__v:0});
+
+    //     if(!user) return res.status(404).json({success:false})
+
+    //     const checkPass = bcrypt.compare(req.query.password,user.password);
+
+    //     if(!checkPass) return res.status(401).json({success:false})
+
+    //     const payload = (user) => {
+    //         return {
+    //             id: user._id,
+    //             fullname: user.fullname,
+    //             email: user.email,
+    //             mobile: String(user.mobile).replace(/\d(?=1\d{2})/g, '*'),
+    //             createdAt: user.createdAt,
+    //         };
+    //     };
+        
+    //     const session = jwt.sign(payload(user),process.env.DATA_SECRET_KEY);
+    //     const {at , rt} = getCookies(undefined, 'all', process.env.DATA_SECRET_KEY ,user); // calling from cookie module
+
+    //     res.cookie('rt',rt,{
+    //         httpOnly:true,
+    //         maxAge:604800000,
+    //         sameSite:'none',
+    //         secure:true
+    //     })
+    
+    //     res.cookie('at',at,{
+    //         httpOnly:true,
+    //         maxAge:900000,
+    //         sameSite:'none',
+    //         secure:true
+    //     })
+    //     const currentTime = moment();
+    //     currentTime.add(12, 'minutes');
+    //     const current_time = btoa(currentTime);
+    //     console.log(current_time);
+    //     res.status(200).json({data:session,time:current_time})
+    // }
+    // catch(err){
+    //     console.log(err);
+    //     res.status(500).json({success:false,message: 'Internal Server Error' })
+    // }
 }
 
 const logout =  (req, res) =>{
