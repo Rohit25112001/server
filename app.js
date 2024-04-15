d1cfde3f9f6f44fe3c79717eb5ada2be4a9399c7
Rohit <rohit.ku25112001@gmail.com>
2024-04-15 20:37:22 +0530

    add user

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const corsConfig = {
-  origin: 'https://rentmojo-eight.vercel.app',
+  origin: 'http://localhost:8080',
   credentials: true
 }
 
